787fcd7bdd088d1d2434df17952ca2653d3f4d88
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2008-02-14 06:41:03 +0000

    More merging
    git-svn-id: https://rorgate.googlecode.com/svn/trunk@8 2d8cff55-da45-0410-a327-c316ea116008

---
+++ RoRGate/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  RoRGate
+//
+//  Created by Juan Germ‡n Casta–eda Echevarr’a on 2/12/08.
+//  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
